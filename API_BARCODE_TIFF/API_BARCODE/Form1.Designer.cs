1ab04b0e6a434718dda3e9f372f6518dc7058990
DiogoGomesGofio16 <diogo.gomes@adp.pt>
2025-04-17 13:10:37 +0100

    api

---
+++ API_BARCODE_TIFF/API_BARCODE/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace API_BARCODE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MSG_LOGS = new System.Windows.Forms.TextBox();
+            this.Pausa = new System.Windows.Forms.Button();
+            this.titulo_label = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Iniciar_Processo = new System.Windows.Forms.Button();
+            this.label_Encerrar = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MSG_LOGS
+            // 
+            this.MSG_LOGS.BackColor = System.Drawing.Color.White;
+            this.MSG_LOGS.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.MSG_LOGS.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MSG_LOGS.ForeColor = System.Drawing.Color.Black;
+            this.MSG_LOGS.Location = new System.Drawing.Point(7, 95);
+            this.MSG_LOGS.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MSG_LOGS.Multiline = true;
+            this.MSG_LOGS.Name = "MSG_LOGS";
+            this.MSG_LOGS.ReadOnly = true;
+            this.MSG_LOGS.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.MSG_LOGS.Size = new System.Drawing.Size(743, 172);
+            this.MSG_LOGS.TabIndex = 0;
+            this.MSG_LOGS.Text = "Processamento em curso...";
+            // 
+            // Pausa
+            // 
+            this.Pausa.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.Pausa.FlatAppearance.BorderSize = 2;
+            this.Pausa.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pausa.ForeColor = System.Drawing.Color.Black;
+            this.Pausa.Location = new System.Drawing.Point(659, 287);
+            this.Pausa.Margin = new System.Windows.Forms.Padding(0);
+            this.Pausa.Name = "Pausa";
+            this.Pausa.Size = new System.Drawing.Size(91, 34);
+            this.Pausa.TabIndex = 1;
+            this.Pausa.Text = "Pausa";
+            this.Pausa.UseVisualStyleBackColor = false;
+            this.Pausa.Visible = false;
+            this.Pausa.Click += new System.EventHandler(this.Pausa_Click);
+            // 
+            // titulo_label
+            // 
+            this.titulo_label.AutoSize = true;
+            this.titulo_label.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titulo_label.ForeColor = System.Drawing.Color.Black;
+            this.titulo_label.Location = new System.Drawing.Point(270, 31);
+            this.titulo_label.Name = "titulo_label";
+            this.titulo_label.Size = new System.Drawing.Size(314, 23);
+            this.titulo_label.TabIndex = 3;
+            this.titulo_label.Text = "Sistema de Digitalização de Processos.";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(7, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(207, 68);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Iniciar_Processo
+            // 
+            this.Iniciar_Processo.BackColor = System.Drawing.Color.White;
+            this.Iniciar_Processo.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.Iniciar_Processo.FlatAppearance.BorderSize = 0;
+            this.Iniciar_Processo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Iniciar_Processo.Location = new System.Drawing.Point(669, 9);
+            this.Iniciar_Processo.Name = "Iniciar_Processo";
+            this.Iniciar_Processo.Size = new System.Drawing.Size(81, 45);
+            this.Iniciar_Processo.TabIndex = 5;
+            this.Iniciar_Processo.UseVisualStyleBackColor = false;
+            this.Iniciar_Processo.Visible = false;
+            // 
+            // label_Encerrar
+            // 
+            this.label_Encerrar.AutoSize = true;
+            this.label_Encerrar.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label_Encerrar.Location = new System.Drawing.Point(271, 296);
+            this.label_Encerrar.Name = "label_Encerrar";
+            this.label_Encerrar.Size = new System.Drawing.Size(304, 18);
+            this.label_Encerrar.TabIndex = 6;
+            this.label_Encerrar.Text = "O programa vai encerrar dentro de 5 segundos...";
+            this.label_Encerrar.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(759, 330);
+            this.Controls.Add(this.label_Encerrar);
+            this.Controls.Add(this.Iniciar_Processo);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.titulo_label);
+            this.Controls.Add(this.Pausa);
+            this.Controls.Add(this.MSG_LOGS);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox MSG_LOGS;
+        private System.Windows.Forms.Button Pausa;
+        private System.Windows.Forms.Label titulo_label;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Iniciar_Processo;
+        private System.Windows.Forms.Label label_Encerrar;
+    }
+}
+
---
+++ API_BARCODE_TIFF/API_BARCODE/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Net.Mail;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using Spire.Barcode;
+using System.Drawing;
+using iTextSharp.text.pdf;
+
+
+namespace API_BARCODE
+{
+    public partial class Form1 : Form
+    {
+        //variavel que vai conter o email do utilizador que está a digitalizar     
+        string Email = "";
+        //varivel que armazena os códigos de barras    
+        string[] Barcode;
+        //mensagem de erro 
+        string Mensagem_Erro = "";
+        //variavel que armazena os ficheiros lidos na pasta de deposito
+        string[] Ficheiros_Retornados;
+        //variavel que armazena o ultimo ficheiro lido
+        string Ultimo_Ficheiro_Executado = "";
+        //localização do ficheiro txt dos logs
+        string Localizacao_Logs_Sucesso = ".\\logs.txt";
+        string Localizacao_Logs_Erro_invalido = ".\\logs.txt";
+        string Localizacao_Logs_Erro_Geral = ".\\logs.txt";
+        //declaraçao da variavel e do tipo de data do timestamp
+        string Data_Hora = DateTime.Now.ToString("dd_MM_yyyy__hh_mm_ss_fff_tt");
+        //conta os erros ao ler os ficheiros
+        int Conta_OS_Erros = 0;
+        //conta os ficheiros lidos com sucesso 
+        int Conta_OS_Sucesso = 0;
+        //conta o total de ficheiros processados 
+        int Conta_Total_Processados = 0;
+        //inicializa a 0 o quantidade de vezes que é efetuado o click no butão de pausa
+        public static int numero_clicks_pausa = 0;
+        //verifica se foi acionado o click do butão 
+        public bool click_botao_pausa = false;
+        //posição em que o array de logs se encontra
+        public int posicao_logs = 0;
+        public int posicao_logs_erro = 0;
+        //linha temporal para permitir a paraguem da aplicação quando é efetuado o click do butão
+        public Thread thread;
+        string outputFilePath;
+        string inputFilePath;
+        public string[] array_mensagens_logs = new string[500];
+        public string[] array_mensagens_logs_erro = new string[500];
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            MSG_LOGS.AppendText(Environment.NewLine);
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            this.Update();
+            this.Iniciar_Processo_Click(sender, e);
+        }
+
+
+        public void Iniciar_Processo_Click(object sender, EventArgs e)
+        {
+            //invoca o ficheiro .ini para leitura da pasta de origem, destino, tipo de ficheiro e pasta de erros
+            var MyIni = new INIFILE(@".\ini_teste.ini");
+            var Pasta_Origem = MyIni.Read("Pasta_Origem");
+            var Pasta_Destino = MyIni.Read("Pasta_Destino");
+            var Tipo_Ficheiro = MyIni.Read("Tipo_Ficheiro");
+            var Pasta_Erros = MyIni.Read("Pasta_Erros");
+
+            //começa um linha de execução que pode ser parada para análise de erros 
+            thread = new Thread(() =>
+            {
+                //vai buscar todo o tipo de ficheiro
+                Ficheiros_Retornados = Directory.GetFiles(Pasta_Origem);
+                //le os ficheiros da pasta de depósito
+                foreach (var Ler_ficheiros in Ficheiros_Retornados)
+                {
+                    try
+                    {//validação do tipo de ficheiro (tif)
+                        if (Path.GetExtension(Ler_ficheiros.ToString()) == Tipo_Ficheiro)
+                        {
+                            //retira o nome do ultimo ficheiro lido 
+                            Ultimo_Ficheiro_Executado = Ler_ficheiros.Substring(Ler_ficheiros.LastIndexOf("Deposito\\") + 9);
+                            //invoca a dll com passaguem de ficheiro por parametro, sendo devolvido o code bar do ficheiro(code 39)
+                            Barcode = BarcodeScanner.Scan(Ler_ficheiros.ToString(), BarCodeType.Code39);
+                            //só move o ficheiro se o barcode for diferente de null
+                            if (Barcode[0] != null)
+                            {
+                                //recolhe o timestamp no momento em que o ficheiro é processado 
+                                Data_Hora = DateTime.Now.ToString("dd_MM_yyyy__hh_mm_ss_fff_tt");
+                                posicao_logs++;
+                                array_mensagens_logs[posicao_logs] = "Date: " + Data_Hora + " | Move file: " + Ler_ficheiros.Substring(Ler_ficheiros.LastIndexOf("Deposito\\") + 9) + " | Barcode: " + Barcode[0].ToString() + ". - SUCCESS";
+                                //move o ficheiro para a pasta de destino colocando o nome o número da OS
+                                File.Move(Ler_ficheiros.ToString(), Pasta_Destino + "\\" + Barcode[0].ToString() + "_OS" + Tipo_Ficheiro);
+
+                            }
+
+                            Conta_OS_Sucesso++;
+                            //apaga o ficheiro tif que foi digitalizado 
+                            File.Delete(Ler_ficheiros);
+                            //apaga o ficheiro pdf com as marcas de texto da api SautinSoft que vai servir para converter o ficheiro tif
+                            File.Delete(Pasta_Destino + "\\" + Barcode[0].ToString() + ".pdf");
+                        }
+                        else
+                        {//caso não existam ficheiros com extensão tif saí da aplicação
+                            break;
+                        }
+                    }
+                    catch (ArgumentException exception_barcode_invalido)
+                    {
+                        //caso exista algum erro no processo será despultada uma mensagem de aviso com o erro
+                        Mensagem_Erro = "Erro: " + exception_barcode_invalido.Message + " Barcode com formato incorreto, ficheiro: " + Ultimo_Ficheiro_Executado + ".";
+                        //recolhe o timestamp no momento em que o erro ocorre 
+                        Data_Hora = DateTime.Now.ToString("dd_MM_yyyy__hh_mm_ss_fff_tt");
+                        posicao_logs_erro++;
+                        array_mensagens_logs_erro[posicao_logs_erro] = "Date: " + Data_Hora + " | Error: " + exception_barcode_invalido.Message + " | Barcode com formato incorreto, no ficheiro " + Ultimo_Ficheiro_Executado + ". - ERROR";
+                        if (File.Exists(Ler_ficheiros))
+                        {
+                            //move o ficheiro para a pasta de erros colocando o timestamp seguido do tipo de ficheiro
+                            File.Move(Ler_ficheiros.ToString(), Pasta_Erros + "\\" + Data_Hora + Tipo_Ficheiro);
+                        }
+                        //envia email para helpdesk
+                        // Send_Email(Mensagem_Erro, Pasta_Erros, Data_Hora);
+                        //escrita do erro nos logs com a mensaguem de erro e o nome do ficheiro 
+                        StreamWriter writer_Logs_Erro_invalido = new StreamWriter(Localizacao_Logs_Erro_invalido, true);
+                        writer_Logs_Erro_invalido.WriteLine("Date: " + Data_Hora + " | Error: " + exception_barcode_invalido.Message + " | Barcode com formato incorreto, no ficheiro " + Ultimo_Ficheiro_Executado + ". - ERROR");
+                        //limpa da memória os dados escritos    
+                        writer_Logs_Erro_invalido.Close();
+                        //permite escrever no objeto (textbox dentro da Thread atual)
+                        MSG_LOGS.Invoke((MethodInvoker)delegate
+                        {
+                            MSG_LOGS.AppendText(Environment.NewLine);
+                            //escreve a mensagem na textbox
+                            MSG_LOGS.AppendText(Mensagem_Erro);
+                            //adiciona uma nova linha na textbox
+                            MSG_LOGS.AppendText(Environment.NewLine);
+                        });
+                        Conta_OS_Erros++;
+                    }
+                    catch (Exception Execao_Geral)
+                    {
+                        //caso exista algum erro no processo será despultada uma mensagem de aviso com o erro
+                        Mensagem_Erro = "Erro: " + Execao_Geral.Message + " | Execao_Geral | Ficheiro: " + Ultimo_Ficheiro_Executado + ".";
+                        //recolhe o timestamp atual no momento em que o erro ocorre 
+                        Data_Hora = DateTime.Now.ToString("dd_MM_yyyy__hh_mm_ss_fff_tt");
+                        posicao_logs_erro++;
+                        array_mensagens_logs_erro[posicao_logs_erro] = "Date: " + Data_Hora + " | Error: " + Execao_Geral.Message + " | Barcode com formato incorreto, no ficheiro " + Ultimo_Ficheiro_Executado + ". - ERROR";
+                        if (File.Exists(Ler_ficheiros))
+                        {
+                            //move o ficheiro para a pasta de erros colocando o timestamp seguido do tipo de ficheiro
+                            File.Move(Ler_ficheiros, Pasta_Erros + "\\" + Data_Hora + Tipo_Ficheiro);
+                        }
+                        //envia email para helpdesk
+                        // Send_Email(Mensagem_Erro, Pasta_Erros, Data_Hora);
+                        //escrita do erro nos logs 
+                        StreamWriter writer_Logs_Erro_Geral = new StreamWriter(Localizacao_Logs_Erro_Geral, true);
+                        writer_Logs_Erro_Geral.WriteLine("Date: " + Data_Hora + " | Error: " + Execao_Geral.Message + " | Ficheiro: " + Ultimo_Ficheiro_Executado + ". - ERROR");
+                        //limpa da memória os dados escritos   
+                        writer_Logs_Erro_Geral.Close();
+                        //permite escrever no objeto (textbox dentro da Thread atual)    
+                        MSG_LOGS.Invoke((MethodInvoker)delegate
+                        {
+                            MSG_LOGS.AppendText(Environment.NewLine);
+                            //escreve na textbox a mensagem de erro
+                            MSG_LOGS.AppendText(Mensagem_Erro);
+                            //adiciona uma linha na textbox 
+                            MSG_LOGS.AppendText(Environment.NewLine);
+                        });
+                        Conta_OS_Erros++;
+                    }
+
+                }
+                for (int i = 0; i <= posicao_logs; i++)
+                {
+                    //StreamWriter writer_Logs_Sucesso = new StreamWriter(Localizacao_Logs_Sucesso, true);
+                    //writer_Logs_Sucesso.WriteLine(array_mensagens_logs[i]);
+                    //writer_Logs_Sucesso.Dispose();
+                    File.AppendAllText(Localizacao_Logs_Sucesso, array_mensagens_logs[i]);
+                    File.AppendAllText(Localizacao_Logs_Sucesso, Environment.NewLine);
+                }
+                for (int i = 0; i <= posicao_logs_erro; i++)
+                {
+                    //StreamWriter writer_Logs_Sucesso = new StreamWriter(Localizacao_Logs_Sucesso, true);
+                    //writer_Logs_Sucesso.WriteLine(array_mensagens_logs[i]);
+                    //writer_Logs_Sucesso.Dispose();
+                    File.AppendAllText(Localizacao_Logs_Erro_Geral, array_mensagens_logs_erro[i]);
+                    File.AppendAllText(Localizacao_Logs_Erro_Geral, Environment.NewLine);
+                }
+
+                //permite escrever no objeto (textbox dentro da Thread atual)    
+                MSG_LOGS.Invoke((MethodInvoker)delegate
+                  {
+                      MSG_LOGS.AppendText(Environment.NewLine);
+                      //mensagem de estado com número de ficheiros corretamente processados 
+                      MSG_LOGS.AppendText("Foram processados " + Conta_OS_Sucesso.ToString() + " ficheiros com sucesso");
+                      MSG_LOGS.AppendText(Environment.NewLine);
+                      //mensagem de estado com número de erros 
+                      MSG_LOGS.AppendText("Foram processados " + Conta_OS_Erros.ToString() + " ficheiros com erro");
+                      MSG_LOGS.AppendText(Environment.NewLine);
+                      //mensagem de estado com o total de erros processados 
+                      Conta_Total_Processados = Conta_OS_Erros + Conta_OS_Sucesso;
+                      MSG_LOGS.AppendText("Foram processados " + Conta_Total_Processados.ToString() + " ficheiros no total");
+                      MSG_LOGS.AppendText(Environment.NewLine);
+                      Pausa.Visible = true;
+                      label_Encerrar.Visible = true;
+                  });
+                Thread.Sleep(5000);
+                Application.Exit();
+            });
+            thread.Start();
+        }
+        private void Pausa_Click(object sender, EventArgs e)
+        {
+            thread.Abort();
+
+        }
+
+
+    }
+}
